5f59d73729cd9e2fff084ad852e518fca96e9bf0
Alex Mellbye <82661985+Alex-Mellbye@users.noreply.github.com>
2021-10-15 15:01:30 +0200

    8th_kyu

--- 8th_kyu.py
+++ 8th_kyu.py
@@ -52,15 +52,86 @@ def simple_multiplication(number) :
     
 def validate_hello(greetings):
     return any(x in greetings.lower() for x in ['hello','ciao','salut','hallo','hola','ahoj','czesc'])
+           
         
+# You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+# Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+# Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
     
-        
+def fix_the_meerkat(arr):
+    arr_new = arr[::-1]
+    return arr_new
+
+
+# It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+# Return the average of the given array rounded down to its nearest integer.
+# The array will never be empty.
+    
+def get_average(marks):
+    import math
+    mean = sum(marks) / len(marks)
+    return math.floor(mean)
+
+
+# Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+# Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+# Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+# "..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+    
+def calculate_age(year_of_birth, current_year):
+    diff = year_of_birth - current_year
+    if diff == -1:
+        return "You are 1 year old."
+    elif diff < -1:
+        return "You are " + str(abs(diff)) + " years old."
+    elif diff == 1:
+        return "You will be born in 1 year."
+    elif diff > 1:
+        return "You will be born in " + str(diff) + " years."
+    else:
+        return "You were born this very year!"
     
     
+# You get any card as an argument. Your task is to return a suit of this card.
+
+def define_suit(card):
+    if "S" in card:
+        return "spades"
+    elif "C" in card:
+        return "clubs"
+    elif "D" in card:
+        return "diamonds"
+    else:
+        return "hearts"
     
     
+# The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+def say_hello(name):
+    return "Hello, " + name
+
+
+# Implement a function which convert the given boolean value into its string representation.
+
+def boolean_to_string(b):
+    if b == True:
+        return "True"
+    else:
+        return "False"
     
     
-    
-    
+# Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+def distinct(seq):
+    return list(dict.fromkeys(seq))
+
+
+# You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
+# You can not use multiplier "*" operator.
+# If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a letter).
+
+def billboard(name, price=30):
+    import numpy as np
+    return np.multiply(len(name), price)
+
     
